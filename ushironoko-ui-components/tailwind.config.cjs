51bd4f8e6ed01c76bcc159bd6086b40c85d3e3ec
vampire-yuta <yuta@vamdemicsystem.black>
2023-01-06 13:45:43 +0900

    fix p104

---
+++ ushironoko-ui-components/tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts}'],
+  darkmode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
